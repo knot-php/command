be406d58eceb643adda3dc918ca6af254d6540ed
Katsuki Shuto <stk2k@sazysoft.com>
2019-12-15 00:27:59 +0900

    Signed-off-by: Katsuki Shuto <stk2k@sazysoft.com>

--- demo/04_execute_command.php
+++ demo/04_execute_command.php
@@ -2,19 +2,19 @@
 require_once __DIR__ . '/include/init.php';
 
 use KnotLib\Di\Container;
-use KnotModule\KnotDi\Adapter\KnotDiContainerAdapter;
+use KnotPhp\Module\KnotDi\Adapter\KnotDiContainerAdapter;
 use KnotPhp\Command\Service\CommandExecService;
-use KnotLib\Kernel\Request\PhpArrayRequest;
 use KnotPhp\Command\Service\CommandDbFileService;
 use KnotPhp\Command\Service\CommandAutoloadService;
 use KnotPhp\Command\Service\AliasDbFileService;
 use KnotPhp\Command\Command\DefaultConsoleIO;
 use KnotPhp\Command\Demo\DemoApplication;
 use KnotPhp\Command\Demo\DemoFileSystemFactory;
+use KnotLib\Console\Request\ShellRequest;
 
 try{
-    $request = new PhpArrayRequest([
-        'Arnold Schwarzenegger', 'Terminator', '--verbose' => true
+    $request = new ShellRequest([
+        'Arnold Schwarzenegger', 'Terminator', '--verbose', '1'
     ]);
     $fs = DemoFileSystemFactory::createFileSystem();
     $app = new DemoApplication($fs);
@@ -41,8 +41,8 @@ try{
     $io->output('---------------------------');
 
     $io->output('Verbose switch off:');
-    $app->request(new PhpArrayRequest([
-        'Michael Jackson', 'MJ', '--verbose' => false
+    $app->request(new ShellRequest([
+        'Michael Jackson', 'MJ', '--verbose', '0'
     ]));
     $exec->executeCommand($di, 'h:w', 0);
 }

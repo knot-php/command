c6e1fc316db0b683e9be6bbf863fe5b034c8f103
Katsuki Shuto <stk2k@sazysoft.com>
2020-02-27 22:34:07 +0900

    Signed-off-by: Katsuki Shuto <stk2k@sazysoft.com>

--- src/Command/Acme/ModuleDependencyExplainComand.php
+++ src/Command/Acme/ModuleDependencyExplainComand.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace KnotPhp\Command\Command\Acme;
 
+use KnotPhp\Command\FileSystem\CommandFileSystem;
 use ReflectionClass, Throwable;
 
 use KnotLib\Kernel\Kernel\ApplicationInterface;
@@ -78,7 +79,9 @@ final class ModuleDependencyExplainComand extends AbstractCommand implements Com
 
         try{
             /** @var ApplicationInterface $app */
-            $app = (new ReflectionClass($app_class))->newInstance();
+            $app = (new ReflectionClass($app_class))->newInstance([
+                new CommandFileSystem()
+            ]);
 
             $app->configure();
 

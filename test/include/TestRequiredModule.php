b3f8cec1c64ccc0737805e8034cfc1a1d589d08f
Katsuki Shuto <stk2k@sazysoft.com>
2020-03-01 01:39:08 +0900

    Signed-off-by: Katsuki Shuto <stk2k@sazysoft.com>

--- test/include/TestRequiredModule.php
+++ test/include/TestRequiredModule.php
@@ -4,12 +4,21 @@ declare(strict_types=1);
 namespace KnotPhp\Command\Test;
 
 use KnotLib\Kernel\Kernel\ApplicationInterface;
-use KnotLib\Kernel\Module\AbstractModule;
 use KnotLib\Kernel\Module\ComponentTypes;
 use KnotLib\Kernel\Module\ModuleInterface;
 
-final class TestRequiredModule extends AbstractModule implements ModuleInterface
+final class TestRequiredModule implements ModuleInterface
 {
+    public static function requiredModules() : array
+    {
+        return [];
+    }
+    
+    public static function requiredComponentTypes() : array
+    {
+        return [];
+    }
+
     public static function declareComponentType(): string
     {
         return ComponentTypes::APPLICATION;
